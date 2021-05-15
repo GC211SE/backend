1f14d0535ddf66d72592dc3d5700e3c19500f9db
DokySp <uhug@naver.com>
2021-05-15 15:20:37 +0000

    [A] Add MySQL Connection

--- app.js
+++ app.js
@@ -3,10 +3,34 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var mysql = require('mysql')
+const cipher = require('./cipher');
+
+
+// var indexRouter = require('./routes/index');
 var signRouter = require('./routes/sign');
 var reservationRouter = require('./routes/reservation');
 var scheduleRouter = require('./routes/schedule');
+
+
+global.conn = mysql.createConnection(
+  {
+    host: cipher.host,
+    user: cipher.user,
+    password: cipher.password,
+    database: cipher.database,
+    multipleStatements: true
+  }
+)
+
+global.updateTime = Date.now()
+
+conn.connect((err) => {
+  if(err != null) console.log("Cannot connect MySQL Server!: \n" + err)
+  else console.log("MySQL Connection Successful!")
+});
+
+console.log("Start at: " + updateTime)
 
 var app = express();
 
@@ -16,9 +40,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/sign', signRouter);
-app.use('/reservation', reservationRouter);
-app.use('/schedule', scheduleRouter);
+// app.use('/', indexRouter);
+app.use('/api/sign', signRouter);
+app.use('/api/reservation', reservationRouter);
+app.use('/api/schedule', scheduleRouter);
 
 module.exports = app;
